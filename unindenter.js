3e88781e0b6de68721627e1ace99a7320b25e242
Leonabcd123 <156839416+Leonabcd123@users.noreply.github.com>
2025-12-13 17:45:38 +0200

    Don't remove characters when indent of first line is more than smaller than other indents

--- unindenter.js
+++ unindenter.js
@@ -5,24 +5,29 @@ function handleSubmit() {
   resultTextArea.value = unindentedString;
 }
 
+function getNumOfLeadingSpaces(string){
+  let i = 0;
+  while (string[i] === " " || string[i] === "\t"){
+      i++;
+  }
+
+  return i;
+}
+
 function unindent(string){
   let lines = string.split("\n");
-  let amountOfSpaces = 0;
+  let numOfSpaces = 0;
 
   for (let i = 0; i < lines.length; i++){
     const currentLine = lines[i];
+    const numOfLeadingSpaces = getNumOfLeadingSpaces(currentLine);
+
     if (i === 0){
-      let j = 0;
-      
-      while (currentLine[j] === " " || currentLine[j] === "\t"){
-        amountOfSpaces++;
-        j++;
-      }
-      
-      if (amountOfSpaces === 0) break;
+      numOfSpaces = numOfLeadingSpaces;
+      if (numOfSpaces === 0) break;
     }
-    
-    lines[i] = currentLine.substring(amountOfSpaces);
+
+    lines[i] = currentLine.substring(numOfLeadingSpaces < numOfSpaces ? numOfLeadingSpaces : numOfSpaces);
   }
 
   return lines.join("\n");
