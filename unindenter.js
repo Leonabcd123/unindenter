3cbaf16001a847174964a15a93221060c22097e0
Leonabcd123 <156839416+Leonabcd123@users.noreply.github.com>
2025-12-13 17:25:38 +0200

    Fix currentLine definition and increment j

--- unindenter.js
+++ unindenter.js
@@ -10,12 +10,13 @@ function unindent(string){
   let amountOfSpaces = 0;
 
   for (let i = 0; i < lines.length; i++){
-    const currentLine = lines[j];
+    const currentLine = lines[i];
     if (i === 0){
       let j = 0;
       
       while (currentLine[j] === " " || currentLine[j] === "\t"){
         amountOfSpaces++;
+        j++;
       }
       
       if (amountOfSpaces === 0) break;
