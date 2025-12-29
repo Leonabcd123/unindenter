677bab367ef071810ce93a80fc518542b80c459e
Leonabcd123 <156839416+Leonabcd123@users.noreply.github.com>
2025-12-29 19:59:52 +0200

    Fix first line not being aligned

--- unindenter.js
+++ unindenter.js
@@ -31,20 +31,16 @@ function getNumOfLeadingSpaces(line, limit){
 
 function unindent(inputString, removeAllIndentation, manualUnindentAmount){
   let lines = inputString.split("\n");
-  let numOfSpaces = manualUnindentAmount ? manualUnindentAmount : 0;
+  let numOfSpaces = manualUnindentAmount && !removeAllIndentation ? manualUnindentAmount : 0;
 
   for (let i = 0; i < lines.length; i++){
     const currentLine = lines[i];
 
     const numOfLeadingSpaces = getNumOfLeadingSpaces(currentLine, numOfSpaces);
 
-    if (i === 0){
-      if (removeAllIndentation){
-        numOfSpaces = 0;
-      } else {
-        numOfSpaces = numOfLeadingSpaces;
-        if (numOfSpaces === 0) break;
-      }
+    if (i === 0 && !removeAllIndentation){
+      numOfSpaces = numOfLeadingSpaces;
+      if (numOfSpaces === 0) break;
     }
 
     lines[i] = currentLine.substring(numOfLeadingSpaces);
